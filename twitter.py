2ae30d3fc8f7f874cdf2687a4f35b35a9f87060a
neelgeek <neelbhave91@gmail.com>
2018-02-02 01:19:46 +0530

    Added %change

--- twitter.py
+++ twitter.py
@@ -2,9 +2,11 @@ import tweepy
 from exchanges import CoinDesk
 from time import sleep
 
+last_price =  8873.72
 
 count =1
 mins = 30
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -15,23 +17,28 @@ def get_price():
     price_usd = round(CoinDesk().get_current_price(currency='USD'), 2)
     price_inr = round(CoinDesk().get_current_price(currency='INR'), 2)
 
+    global last_price
+    per_change = ((float(price_usd) - last_price) / float(price_usd)) * 100
+    per_change = round(per_change, 2)
+    last_price = float(price_usd)
+
     bitcoin_rate = ''' 
-    in USD : '''+ str(price_usd) +'''$
-    in INR : '''+str(price_inr)+''' Rs
-    
-    #Bitcoin #Crypto #Cryptocurrency
-    '''
+       in USD : ''' + str(price_usd) + '''$
+       in INR : ''' + str(price_inr) + ''' Rs
+
+       %age change since last update = ''' + str(per_change) + '''%
+
+       #Bitcoin #Crypto #Cryptocurrency
+       '''
     return bitcoin_rate
 
-
 api.update_status("#"+str(count)+" Latest Bitcoin Price is " + get_price())
-print("#"+str(count)+" Status posted so far :" + str(count))
+print("Status posted so far :" + str(count))
 count+=1
 while True:
     sleep(30*mins)
     api.update_status("#"+str(count)+" Latest Bitcoin Price is "+get_price())
-    print("#"+str(count)+" Status posted so far :" + str(count))
+    print("Status posted so far :" + str(count))
     count+=1
-
 
 
